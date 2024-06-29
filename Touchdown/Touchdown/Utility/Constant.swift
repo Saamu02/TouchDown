435d9c06978c478e8845e8f56404d93b1524d88b
Saamu02 <iusamairfan@gmail.com>
2024-06-29 13:04:05 +0500

    Assets added

---
+++ Touchdown/Touchdown/Utility/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Ussama Irfan on 29/06/2024.
+//
+
+import SwiftUI
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4) 
